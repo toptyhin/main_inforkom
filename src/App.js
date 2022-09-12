7baa9b332f74c184cd77b56203e0b30d58b3aa0d
alk <konovalov.phone@gmail.com>
2022-09-12 15:27:30 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+	
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Header from './components/header';
+import Footer from './components/footer';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <main className="container">
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/dashboard" element={<About />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
     </div>
+
   );
 }
 
