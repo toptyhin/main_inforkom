0276d8369a48cc8c2bd4cc682f54b0b12e5c2d30
Alina Sukhova <alina.p.sukhova@gmail.com>
2022-12-21 13:49:36 +0300

    custom search

--- src/pages/map-stations/map-station/map/map.js
+++ src/pages/map-stations/map-station/map/map.js
@@ -1,21 +1,26 @@
 import MapPoint from './children/mapPoint';
-import { Map, ObjectManager, ZoomControl } from '@pbe/react-yandex-maps';
+import { Map, ObjectManager, ZoomControl, SearchControl, ListBox, ListBoxItem } from '@pbe/react-yandex-maps';
 
 const InforkomMap = () => {
-    return (
-        <Map 
-        className={'map'}
-        defaultState={{ 
-          center: [55.75, 37.57], 
-          zoom: 9,
-        }} 
-        width="100%"
-        height="100vh"
-        >
-        <ZoomControl options={{ float: "right" }} />
-        </Map>
-    );
-  }
+  return (
+    <Map 
+      className={'map'}
+      defaultState={{ 
+      center: [55.75, 37.57], 
+      zoom: 9,
+      }}
+      width="100%"
+      height="100vh"
+    >
+      <ZoomControl options={{ float: "right" }} />
+      <SearchControl options={{ placeholderContent: "Поиск" }} />
+      <ListBox data={{ content: "Фильтр по топливу" }} >
+        <ListBoxItem data={{ content: "ГАЗ" }} />
+        <ListBoxItem data={{ content: "ДТ" }} />
+      </ListBox>
+    </Map>
+  );
+}
 
-  export default InforkomMap;
+export default InforkomMap;
   
