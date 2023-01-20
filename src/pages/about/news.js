aac9895175f4fb80748acc2e8fbcbdf3549ddf81
Alina Sukhova <alina.p.sukhova@gmail.com>
2023-01-20 11:33:56 +0300

    dangerouslySetInnerHTML

--- src/pages/about/news.js
+++ src/pages/about/news.js
@@ -1,20 +1,9 @@
-import TextAreaHead from '../../components/basicPage/textArea/textAreaHead'
-import TextAreaHeadL from '../../components/basicPage/textArea/textAreaHeadL'
-import TextAreaText from '../../components/basicPage/textArea/textAreaText'
-import TextAreaList from '../../components/basicPage/textArea/textAreaList'
-import TextAreaSpecial from '../../components/basicPage/textArea/textAreaSpecial'
-import SideAreaMenu from '../../components/basicPage/sideArea/sideAreaMenu'
-import PageHeader from '../../components/basicPage/pageHeader/pageHeader';
-import JsxParser from 'react-jsx-parser'
-
 function News({children}) {
   return (
-    <div className='main'>
-      <JsxParser
-          components={{ PageHeader, TextAreaHead, TextAreaHeadL, TextAreaText, TextAreaSpecial }}
-          jsx={children}
-        />
-    </div>
+    <div className='main'
+      dangerouslySetInnerHTML={{__html: children}}
+       
+    />
   )}
-  
+   
 export default News;
